47ce7f9b9fad90d6d8f817f3a0ebfe0b5e4a3848
Nataliia-Homyk324 <homyk324@gmail.com>
2024-01-31 13:11:32 +0200

    alert

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,24 +4,29 @@ const textarea = document.querySelector('textarea[name="message"]');
 
 
 const localStorageKey = 'feedback-form-state';
+
+
+
 if (localStorage.getItem(localStorageKey)) {
   // Отримуємо об'єкт зі збереженими даними
   const savedData = JSON.parse(localStorage.getItem(localStorageKey));
   
   // Заповнюємо поля форми зі збереженими даними
-  input.value = savedData.mail;
-  textarea.value = savedData.massage;
+  input.value = savedData.email;
+  textarea.value = savedData.message;
 }
+
+
 
 form.addEventListener('input', (event) => {
   // Отримуємо значення поля вводу та текстової області
-  const mail = input.value.trim();
-  const massage = textarea.value.trim();
+  const email = input.value.trim();
+  const message = textarea.value.trim();
   
   // Створюємо об'єкт feedback
   const feedback = {
-    mail: mail,
-    massage: massage
+    email: email,
+    message: message
   };
   //Зберігаємо об'єкт в локальному сховищі
     localStorage.setItem(localStorageKey, JSON.stringify(feedback));
@@ -32,8 +37,13 @@ form.addEventListener('submit', event => {
     event.preventDefault();
     const email = input.value;
     const message = textarea.value;
+  
+  if (email && message) {
     console.log({email, message});
     input.value = '';
     textarea.value = '';
     localStorage.removeItem(localStorageKey);
+  } else {
+    alert('Будь ласка, заповніть усі поля');
+  }
 })
